8396a7d79616a53a9f59b372b8243355b91ea686
Cristobal Pina <mondmaxy@gmail.com>
2020-05-23 18:17:34 -0400

    first commit

---
+++ src/App/Pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <div>
+      <h1>Pages</h1>
+    </div>
+  )
+}
+
+export default Pages
---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Pages from './Pages'
+
+const App = () => {
+  return (
+    <>
+      <Pages />
+    </>
+  )
+}
+
+export default App
